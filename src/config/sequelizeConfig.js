87821179512421e9368d7a3d0c6f27e8e3b6cb1a
Nha1213 <Vothanarern@gmail.com>
2026-03-02 23:29:33 +0700

    update

--- src/config/sequelizeConfig.js
+++ src/config/sequelizeConfig.js
@@ -3,13 +3,20 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
   process.env.DB_NAME || 'backend_pos_api',
-  process.env.DB_USER || 'root',
-  process.env.DB_PASSWORD || '',
   {
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 3306,
-    dialect: process.env.DB_DIALECT || 'mysql'
+    dialect: "mysql",
   }
 );
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME || 'backend_pos_api',
+//   process.env.DB_USER || 'root',
+//   process.env.DB_PASSWORD || '',
+//   {
+//     host: process.env.DB_HOST || 'localhost',
+//     port: process.env.DB_PORT || 3306,
+//     dialect: process.env.DB_DIALECT || 'mysql'
+//   }
+// );
 
 module.exports = sequelize;
